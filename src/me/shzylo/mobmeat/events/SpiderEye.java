d99fb44712c87421b733b918ce4fbf4a191ae20f
Shzylo <shzylo66@gmail.com>
2013-08-10 00:42:18 -0500

    Update SpiderEye.java

--- src/me/shzylo/mobmeat/events/SpiderEye.java
+++ src/me/shzylo/mobmeat/events/SpiderEye.java
@@ -2,7 +2,9 @@ package me.shzylo.mobmeat.events;
 
 import me.shzylo.mobmeat.MobMeat;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,40 +14,58 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+/* =====
+ * SpiderEye Features:
+ * - Craft in 2x2 square to make fermented.
+ * - Creates slow effect when ate.
+ * =====
+ */
+
 public class SpiderEye implements Listener {
 
-  MobMeat plugin;
+	MobMeat plugin;
+
+	private boolean newEffectEnabled, newCraftRecipe;
 
 	public SpiderEye(MobMeat instance) {
 		this.plugin = instance;
-		boolean enableCraft = false;
 
-		if (plugin.getConfig().getBoolean("craft-spidereye", true) == true)
-			enableCraft = true;
+		newCraftRecipe = plugin.getConfig().getBoolean("craft-spidereye", true);
+		newEffectEnabled = plugin.getConfig().getBoolean("new-eat-effect", true);
 
-		if (enableCraft) {
-			ShapedRecipe fermentedEye = new ShapedRecipe(new ItemStack(Material.FERMENTED_SPIDER_EYE, 1))
-					.shape("ee", "ee").setIngredient('e', Material.SPIDER_EYE);
+		if (newCraftRecipe) {
+			ShapedRecipe fermentedEye = new ShapedRecipe(new ItemStack(Material.FERMENTED_SPIDER_EYE, 1)).shape("ee", "ee")
+					.setIngredient('e', Material.SPIDER_EYE);
 			plugin.getServer().addRecipe(fermentedEye);
 		}
+	}
+
+	public void getInfo(CommandSender s) {
+		if (newEffectEnabled) {
+			s.sendMessage("");
+			s.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "SPIDEREYE");
+			s.sendMessage("- " + ChatColor.LIGHT_PURPLE + "Craft spider eyes in a 2 by 2 square to make a "
+					+ "fermented spider eye.");
+			s.sendMessage("- " + ChatColor.LIGHT_PURPLE
+					+ "Eating a spider eye takes away the poison effect, but a 10 second slow effect and 5 second confusion, and "
+					+ "it adds one extra point of hunger.");
+		} else
+			s.sendMessage(ChatColor.GREEN + "" + ChatColor.ITALIC + "This Feature is currently off");
 	}
 
 	@EventHandler
 	public void eatSpiderEye(PlayerItemConsumeEvent e) {
 		final Player p = e.getPlayer();
 
-		boolean enabled = false;
-		if (plugin.getConfig().getBoolean("new-eat-effect", true) == true)
-			enabled = true;
-
-		if (enabled) {
+		if (newEffectEnabled) {
 			if (p.getItemInHand().getType().equals(Material.SPIDER_EYE)) {
 				if (p.getFoodLevel() < 20) {
 					p.setFoodLevel(p.getFoodLevel() + 1);
 					p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 3));
+					p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 3));
 
 					if (p.getHealth() < 20) {
-						int health = p.getHealth();
+						double health = p.getHealth();
 						p.setHealth(health + 1);
 					}
 
